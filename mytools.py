43aeb5fecb120251bf1177f3f4532a8dcaa982a0
xuguyuan <247180346@qq.com>
2019-09-02 00:31:48 -0400

    initial upload and minor changes to the comments

---
+++ mytools.py
@@ -0,0 +1,27 @@
+
+class string:
+    def __init__(self):
+        print("tools: path_conver")
+
+    def tips():
+        print("string processing list:\n"+
+        "1) path_convert(): include the original pth to the function()"
+        
+        
+        ) #you can still add more features to the class, remember t0 add a tips describing the function calling methos
+
+
+    def path_convert(call=False): #if you do not know how to call, just paste the original path the the function ()
+        original_path = ''
+        if call==False:           #this branch for cases that you forget to include the path to the function ()
+            print("ERRO WITH VSCODE! pls directly input the path in the function")
+        else:
+            original_path = call
+            path_output=original_path.replace("\\","\\\\")
+            print("\n\nreal ouput list down below\n\n"+path_output+"\n")
+
+        #123 testing for git
+
+
+
+
